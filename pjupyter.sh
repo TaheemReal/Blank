252293ba8871ae0e64e61af30488576d589cb6e7
TaheemReal <132823975+TaheemReal@users.noreply.github.com>
2023-09-25 21:59:35 +0530

    Delete pjupyter.sh

--- pjupyter.sh
+++
@@ -1,9 +0,0 @@
-set password "lollipop"
-set expect_path "/usr/bin/expect"  ;# Replace with the actual path to the 'expect' command
-
-spawn jupyter notebook passowrd
-expect "Password:"
-send "$password\r"
-expect "Verify:"
-send "$password\r"
-interact
